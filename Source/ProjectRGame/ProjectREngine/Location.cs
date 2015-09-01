5a9fa258efdeedc51228b72c052e229bca476633
ggilbert108 <ggilbert1@harding.edu>
2015-09-01 16:11:55 -0500

    Init Commit
    Init Commit

---
+++ Source/ProjectRGame/ProjectREngine/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectREngine
+{
+    public struct Location
+    {
+        public int r;
+        public int c;
+
+        public Location(int r, int c)
+        {
+            this.r = r;
+            this.c = c;
+        }
+    }
+}
